cc4886769038955cc53bb016aad6dac247ea3f81
ah-nd-naf <ahnaf.rasheed.zaki@gmail.com>
2026-04-29 13:41:09 +0600

    Initial commit: PERN stack structure and DB connection

---
+++ server/db.js
@@ -0,0 +1,26 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+
+});
+
+// testing if it works
+
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error acquiring client', err.stack);
+    }
+    console.log("Database connected successfully");
+    release();
+});
+
+module.exports = pool;
+
+
+
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db"); // import the db connection
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server is running! ");
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
